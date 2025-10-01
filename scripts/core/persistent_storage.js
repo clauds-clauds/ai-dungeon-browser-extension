8e7b7b2cf221820a545b7bb36545a4b55eb19b80
clauds-clauds <217328249+clauds-clauds@users.noreply.github.com>
2025-10-01 22:42:54 +0200

    add all missing settings back

--- scripts/core/persistent_storage.js
+++ scripts/core/persistent_storage.js
@@ -28,21 +28,90 @@ class PersistentStorage {
      * The default settings.
      */
     static #defaults = {
-        textEffectsEnabled: true,
+        /* Text Effects HERE! */
+        textEffectsIcons: true,
         textEffectsColor: true,
         textEffectsBold: true,
         textEffectsStory: true,
         textEffectsDialogue: true,
-        textEffectsIcon: true,
         textEffectsIconSize: 28,
         textEffectsIconBorderWidth: 1,
         textEffectsIconBorderRadius: 0,
-        textEffectsColorGlobal: 'rgba(200, 170, 100, 1)',
+        textEffectsColor: "#f8ae2c",
 
-        tooltipEnabled: true,
-        tooltipScale: 25,
-        tooltipHideDelay: 400
+        /* Tooltip HERE! */
+        tooltipGraphics: true,
+        tooltipGraphicSize: 20,
+        tooltipHideDelay: 200
     };
+
+    /**
+     * Retrieves a setting value from the cache or defaults.
+     * @param {*} setting The setting key to retrieve.
+     * @param {*} defaultValue The default value to return if the setting is not found.
+     * @returns {*} The setting value or the provided default value.
+     */
+    static getSetting(setting, defaultValue = null) {
+        return this.cache.settings[setting] ?? this.#defaults[setting] ?? defaultValue;
+    }
+
+    /**
+     * Saves a setting to the cache and database.
+     * @param {*} setting 
+     * @param {*} value 
+     * @return {Promise<void>}
+     */
+    static async saveSetting(setting, value){
+        this.cache.settings[setting] = value;
+
+        document.dispatchEvent(new CustomEvent(Configuration.EVENT_CACHE_UPDATED));
+
+        await this.#db.settings.put({
+            key: Configuration.DATABASE_KEY,
+            value: this.cache.settings
+        });
+
+        CustomDebugger.say(`Saved setting ${setting} with value ${value}.`, true);
+    }
+
+    /**
+     * Saves an entity to the database and updates the cache.
+     * @param {*} entity The entity to save.
+     * @returns {Promise<void>}
+     */
+    static async saveEntity(entity) {
+        if (!entity) return;
+        await this.#db.entities.put(entity);
+
+        const safeEntity = JSON.parse(JSON.stringify(entity));
+        const index = this.cache.entities.findIndex(e => e.id === safeEntity.id);
+
+        if (index > -1) this.cache.entities[index] = safeEntity;
+        else this.cache.entities.push(safeEntity);
+
+        document.dispatchEvent(new CustomEvent(Configuration.EVENT_CACHE_UPDATED));
+        CustomDebugger.say(`Saved entity ${entity.id} (${entity.type}).`, true);
+    }
+
+    /**
+     * Deletes an entity from the database and cache.
+     * @param {*} entity The entity to delete.
+     * @return {Promise<void>}
+     */
+    static async deleteEntity(entity) {
+        try {
+            await this.#db.entities.delete(entity); // Delete from database.
+
+            const index = this.cache.entities.findIndex(e => e.id === entity);
+            if (index > -1) this.cache.entities.splice(index, 1);
+
+            document.dispatchEvent(new CustomEvent(Configuration.EVENT_CACHE_UPDATED));
+            CustomDebugger.say(`Deleted entity ${entity}`, true);
+        } catch (error) {
+            CustomDebugger.scream('Failed to delete entity from storage!');
+            throw error;
+        }
+    }
 
     /**
      * Pings the persistent storage for a specific adventure.
